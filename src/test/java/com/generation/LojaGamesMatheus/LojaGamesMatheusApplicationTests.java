f1828c9f3cc3708f25eb5634000d78034b67a987
Matheus094-tech <80859363+Matheus094-tech@users.noreply.github.com>
2021-06-02 14:46:03 -0300

    Games

---
+++ src/test/java/com/generation/LojaGamesMatheus/LojaGamesMatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.LojaGamesMatheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesMatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
